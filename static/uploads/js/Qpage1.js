eff1cc8c5cac08a05a7cec09d3c19cbd1eba4d60
yael <dote.dg.p@gmail.com>
2025-12-18 09:32:28 +0200

    blue model connected

--- static/uploads/js/Qpage1.js
+++ static/uploads/js/Qpage1.js
@@ -24,7 +24,6 @@ function showDisabilityDetails() {
     document.getElementById("disability").style.display = "block";
 } 
 
-<<<<<<< HEAD
 function blueM(){
     window.location.href = "/blueM";
 }
@@ -32,8 +31,3 @@ function blueM(){
 function pinkM(){
     window.location.href = "/pinkM";
 }
-=======
-function models(){
-    
-}
->>>>>>> b87d363e7cc9471cec8f88e839c0a0338c3ee30c
